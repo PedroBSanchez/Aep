e3bce2b0af8a77cb406f2d77e75b080a622050d2
PedroBSanchez <pedro_b47@hotmail.com>
2020-09-22 17:37:09 -0300

    att

--- Aep/src/aep/Prioridade.java
+++ Aep/src/aep/Prioridade.java
@@ -6,5 +6,32 @@ public class Prioridade {
     private int nivel;
     private int idPrioridade;
 
+    
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getIdPrioridade() {
+        return idPrioridade;
+    }
+
+    public void setIdPrioridade(int idPrioridade) {
+        this.idPrioridade = idPrioridade;
+    }
+
+
+    
 
 }
